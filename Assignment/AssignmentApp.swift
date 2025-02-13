c044fe1c9b5a39bd29e68ffe4f2b1fab3ed37d07
pravalgautam <71179696+pravalgautam@users.noreply.github.com>
2025-02-13 18:09:18 +0530

    Initial Commit

---
+++ Assignment/AssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentApp.swift
+//  Assignment
+//
+//  Created by Praval Gautam on 13/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
